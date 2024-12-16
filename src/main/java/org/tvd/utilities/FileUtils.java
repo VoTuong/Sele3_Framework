bc355f0c346d1400b9d4d39e6b8265489be30806
dtuong.vo <dtuong.vo@gmail.com>
2024-12-16 23:45:33 +0700

    init project

---
+++ src/main/java/org/tvd/utilities/FileUtils.java
@@ -0,0 +1,10 @@
+package org.tvd.utilities;
+
+import java.io.File;
+
+public class FileUtils {
+
+    public static String getCurrentDir() {
+        return System.getProperty("user.dir") + File.separator;
+    }
+}
---
+++ src/main/java/org/tvd/utilities/PropertiesUtils.java
@@ -0,0 +1,32 @@
+package org.tvd.utilities;
+
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class PropertiesUtils {
+
+    private static Properties properties;
+    private static FileInputStream file;
+
+    private static final String propertiesFilePath = "src/test/resources/properties/configs.properties";
+
+    public static String getValue(String key) {
+        String keyValue = null;
+        try {
+            if (file == null) {
+                properties = new Properties();
+                String linkFile = FileUtils.getCurrentDir() + propertiesFilePath;
+                file = new FileInputStream(linkFile);
+                properties.load(file);
+                file.close();
+            }
+            // Get value from file
+            keyValue = properties.getProperty(key);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return keyValue;
+    }
+
+}
